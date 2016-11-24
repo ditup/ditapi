0c7295b42c7638e5e3c56cfe100e8033160018dd
mrkvon <michal.salajka@email.cz>
2016-11-24 19:32:52 +0100

    added Access-Control-Allow-Methods header

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ app.use(function (req, res, next) {
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
   next();
 });
 
