5bab0c1d7ac3965a65ae972fde018cb1133628dc
mrkvon <michal.salajka@email.cz>
2016-12-13 20:55:39 +0100

    refactor co(generator) to async

--- test/tags.js
+++ test/tags.js
@@ -3,8 +3,7 @@
 let supertest = require('supertest'),
     should = require('should'),
     path = require('path'),
-    _ = require('lodash'),
-    co = require('co');
+    _ = require('lodash');
 
 let app = require(path.resolve('./app')),
     serializers = require(path.resolve('./serializers')),
@@ -24,17 +23,17 @@ let dbData,
 describe('/tags', function () {
 
   // clear database after every test
-  afterEach(function () {
-    return co(function* () {
-      yield dbHandle.clear();
-    });
+  afterEach(async function () {
+    await dbHandle.clear();
   });
 
   describe('GET', function () {
     it('should show lists of tags');
 
-    context('?search=string', function () {
-      it('match tagnames');
+    context('?filter[tagname][like]=string', function () {
+      it('match tags with similar tagnames', async function () {
+        throw new Error('todo');
+      });
     });
   });
 
@@ -55,134 +54,82 @@ describe('/tags', function () {
     let serializedInvalidTagname = serialize.newTag(invalidTagname);
 
     // put pre-data into database
-    beforeEach(function () {
-      return co(function* () {
-        let data = {
-          users: 3, // how many users to make
-          verifiedUsers: [0, 1], // which  users to make verified
-          tags: 1
-        }
-        // create data in database
-        dbData = yield dbHandle.fill(data);
+    beforeEach(async function () {
+      let data = {
+        users: 3, // how many users to make
+        verifiedUsers: [0, 1], // which  users to make verified
+        tags: 1
+      }
+      // create data in database
+      dbData = await dbHandle.fill(data);
 
-        loggedUser = dbData.users[0];
-      });
+      loggedUser = dbData.users[0];
     });
 
     context('logged in', function () {
-      it('[good data] should create a tag and respond with 201', function () {
-        return co(function* () {
-          let response = yield new Promise(function (resolve, reject) {
-            agent
-              .post(`/tags`)
-              .send(serializedNewTag)
-              .set('Content-Type', 'application/vnd.api+json')
-              .set('Authorization', 'Basic '+
-                new Buffer(`${loggedUser.username}:${loggedUser.password}`)
-                  .toString('base64'))
-              .expect(201)
-              .expect('Content-Type', /^application\/vnd\.api\+json/)
-              .end(function (err, res) {
-                if (err) return reject(err);
-                return resolve(res);
-              });
-          });
+      it('[good data] should create a tag and respond with 201', async function () {
+        let response = await agent
+            .post(`/tags`)
+            .send(serializedNewTag)
+            .set('Content-Type', 'application/vnd.api+json')
+            .auth(loggedUser.username, loggedUser.password)
+            .expect(201)
+            .expect('Content-Type', /^application\/vnd\.api\+json/);
 
-          // check that the newly created tag is there
-          let tag = yield models.tag.read(newTag.tagname);
+        // check that the newly created tag is there
+        let tag = await models.tag.read(newTag.tagname);
 
-          (typeof tag).should.equal('object');
-          tag.should.have.property('tagname', newTag.tagname);
-          tag.should.have.property('description', newTag.description);
-          tag.should.have.property('creator');
-          tag.creator.should.have.property('username', loggedUser.username);
-        });
+        (typeof tag).should.equal('object');
+        tag.should.have.property('tagname', newTag.tagname);
+        tag.should.have.property('description', newTag.description);
+        tag.should.have.property('creator');
+        tag.creator.should.have.property('username', loggedUser.username);
       });
 
-      it('[invalid tagname] should error with 400', function () {
-        return co(function* () {
-          let response = yield new Promise(function (resolve, reject) {
-            agent
-              .post(`/tags`)
-              .send(serializedInvalidTagname)
-              .set('Content-Type', 'application/vnd.api+json')
-              .set('Authorization', 'Basic '+
-                new Buffer(`${loggedUser.username}:${loggedUser.password}`)
-                  .toString('base64'))
-              .expect(400)
-              .expect('Content-Type', /^application\/vnd\.api\+json/)
-              .end(function (err, res) {
-                if (err) return reject(err);
-                return resolve(res);
-              });
-          });
-        });
+      it('[invalid tagname] should error with 400', async function () {
+        let response = await agent
+          .post(`/tags`)
+          .send(serializedInvalidTagname)
+          .set('Content-Type', 'application/vnd.api+json')
+          .auth(loggedUser.username, loggedUser.password)
+          .expect(400)
+          .expect('Content-Type', /^application\/vnd\.api\+json/);
       });
 
-      it('[duplicate tagname] should error with 409', function () {
-        return co(function* () {
-          let response = yield new Promise(function (resolve, reject) {
-            agent
-              .post(`/tags`)
-              .send(serialize.newTag(dbData.tags[0]))
-              .set('Content-Type', 'application/vnd.api+json')
-              .set('Authorization', 'Basic '+
-                new Buffer(`${loggedUser.username}:${loggedUser.password}`)
-                  .toString('base64'))
-              .expect(409)
-              .expect('Content-Type', /^application\/vnd\.api\+json/)
-              .end(function (err, res) {
-                if (err) return reject(err);
-                return resolve(res);
-              });
-          });
-        });
+      it('[duplicate tagname] should error with 409', async function () {
+        let response = await agent
+          .post(`/tags`)
+          .send(serialize.newTag(dbData.tags[0]))
+          .set('Content-Type', 'application/vnd.api+json')
+          .auth(loggedUser.username, loggedUser.password)
+          .expect(409)
+          .expect('Content-Type', /^application\/vnd\.api\+json/);
       });
 
-      it('[invalid description]', function () {
+      it('[invalid description]', async function () {
         let longDescription = _.repeat('.', 2049);
-        return co(function* () {
-          let response = yield new Promise(function (resolve, reject) {
-            agent
-              .post(`/tags`)
-              .send(serialize.newTag({
-                tagname: 'new-tag',
-                description: longDescription
-              }))
-              .set('Content-Type', 'application/vnd.api+json')
-              .set('Authorization', 'Basic '+
-                new Buffer(`${loggedUser.username}:${loggedUser.password}`)
-                  .toString('base64'))
-              .expect(400)
-              .expect('Content-Type', /^application\/vnd\.api\+json/)
-              .end(function (err, res) {
-                if (err) return reject(err);
-                return resolve(res);
-              });
-          });
-        });
+        let response = await agent
+          .post(`/tags`)
+          .send(serialize.newTag({
+            tagname: 'new-tag',
+            description: longDescription
+          }))
+          .set('Content-Type', 'application/vnd.api+json')
+          .auth(loggedUser.username, loggedUser.password)
+          .expect(400)
+          .expect('Content-Type', /^application\/vnd\.api\+json/);
       });
     });
 
     context('not logged in', function () {
-      it('should say 403 Forbidden', function () {
-        return co(function* () {
-          let response = yield new Promise(function (resolve, reject) {
-            agent
-              .post(`/tags`)
-              .send(serializedNewTag)
-              .set('Content-Type', 'application/vnd.api+json')
-              .set('Authorization', 'Basic '+
-                new Buffer(`${loggedUser.username}:${loggedUser.password}a`)
-                  .toString('base64'))
-              .expect(403)
-              .expect('Content-Type', /^application\/vnd\.api\+json/)
-              .end(function (err, res) {
-                if (err) return reject(err);
-                return resolve(res);
-              });
-          });
-        });
+      it('should say 403 Forbidden', async function () {
+        let response = await agent
+          .post(`/tags`)
+          .send(serializedNewTag)
+          .set('Content-Type', 'application/vnd.api+json')
+          .auth(loggedUser.username, `${loggedUser.password}a`)
+          .expect(403)
+          .expect('Content-Type', /^application\/vnd\.api\+json/);
       });
     });
   });
@@ -190,103 +137,70 @@ describe('/tags', function () {
 
 describe('/tags/:tagname', function () {
   // put pre-data into database
-  beforeEach(function () {
-    return co(function* () {
-      let data = {
-        users: 3, // how many users to make
-        verifiedUsers: [0, 1], // which  users to make verified
-        tags: 7
-      }
-      // create data in database
-      dbData = yield dbHandle.fill(data);
+  beforeEach(async function () {
+    let data = {
+      users: 3, // how many users to make
+      verifiedUsers: [0, 1], // which  users to make verified
+      tags: 7
+    }
+    // create data in database
+    dbData = await dbHandle.fill(data);
 
-      loggedUser = dbData.users[0];
-    });
+    loggedUser = dbData.users[0];
   });
+
   // clear database after every test
-  afterEach(function () {
-    return co(function* () {
-      yield dbHandle.clear();
-    });
+  afterEach(async function () {
+    await dbHandle.clear();
   });
 
   describe('GET', function () {
-    it('should show the tag', function () {
+    it('should show the tag', async function () {
       let existentTag = dbData.tags[0];
-      return co(function* () {
-        let response = yield new Promise(function (resolve, reject) {
-          agent
-            .get(`/tags/${existentTag.tagname}`)
-            .set('Content-Type', 'application/vnd.api+json')
-            .set('Authorization', 'Basic '+
-              new Buffer(`${loggedUser.username}:${loggedUser.password}`)
-                .toString('base64'))
-            .expect(200)
-            .expect('Content-Type', /^application\/vnd\.api\+json/)
-            .end(function (err, res) {
-              if (err) return reject(err);
-              return resolve(res);
-            });
-        });
+      let response = await agent
+        .get(`/tags/${existentTag.tagname}`)
+        .set('Content-Type', 'application/vnd.api+json')
+        .auth(loggedUser.username, loggedUser.password)
+        .expect(200)
+        .expect('Content-Type', /^application\/vnd\.api\+json/);
 
-        let tag = response.body;
+      let tag = response.body;
 
-        tag.should.have.property('data');
-        tag.data.should.have.property('id', existentTag.tagname);
-        tag.data.should.have.property('attributes');
+      tag.should.have.property('data');
+      tag.data.should.have.property('id', existentTag.tagname);
+      tag.data.should.have.property('attributes');
 
-        let attrs = tag.data.attributes;
-        attrs.should.have.property('tagname', existentTag.tagname);
-        attrs.should.have.property('description', existentTag.description);
+      let attrs = tag.data.attributes;
+      attrs.should.have.property('tagname', existentTag.tagname);
+      attrs.should.have.property('description', existentTag.description);
 
-        // TODO figure out JSON API creator & contributors...
-      });
+      // TODO figure out JSON API creator & contributors...
     });
 
     it('show creator'); // as a json api relation
 
-    it('[nonexistent tagname] should error 404', function () {
+    it('[nonexistent tagname] should error 404', async function () {
       let existentTag = dbData.tags[0];
-      return co(function* () {
-        let response = yield new Promise(function (resolve, reject) {
-          agent
-            .get(`/tags/nonexistent-tag`)
-            .set('Content-Type', 'application/vnd.api+json')
-            .set('Authorization', 'Basic '+
-              new Buffer(`${loggedUser.username}:${loggedUser.password}`)
-                .toString('base64'))
-            .expect(404)
-            .expect('Content-Type', /^application\/vnd\.api\+json/)
-            .end(function (err, res) {
-              if (err) return reject(err);
-              return resolve(res);
-            });
-        });
+      let response = await agent
+        .get(`/tags/nonexistent-tag`)
+        .set('Content-Type', 'application/vnd.api+json')
+        .auth(loggedUser.username, loggedUser.password)
+        .expect(404)
+        .expect('Content-Type', /^application\/vnd\.api\+json/)
 
-        response.body.should.have.property('errors');
-      });
+      response.body.should.have.property('errors');
     });
 
-    it('[invalid tagname] should error 400', function () {
+    it('[invalid tagname] should error 400', async function () {
       let existentTag = dbData.tags[0];
-      return co(function* () {
-        let response = yield new Promise(function (resolve, reject) {
-          agent
-            .get(`/tags/invalid_tag`)
-            .set('Content-Type', 'application/vnd.api+json')
-            .set('Authorization', 'Basic '+
-              new Buffer(`${loggedUser.username}:${loggedUser.password}`)
-                .toString('base64'))
-            .expect(400)
-            .expect('Content-Type', /^application\/vnd\.api\+json/)
-            .end(function (err, res) {
-              if (err) return reject(err);
-              return resolve(res);
-            });
-        });
+      let response = await agent
+        .get(`/tags/invalid_tag`)
+        .set('Content-Type', 'application/vnd.api+json')
+        .auth(loggedUser.username, loggedUser.password)
+        .expect(400)
+        .expect('Content-Type', /^application\/vnd\.api\+json/);
 
-        response.body.should.have.property('errors');
-      });
+      response.body.should.have.property('errors');
     });
   });
 
