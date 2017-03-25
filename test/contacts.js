3a3cf471aef225f4e04cd99f31a5b987abcf56d7
mrkvon <michal.salajka@protonmail.com>
2017-03-25 23:13:38 +0100

    start writing tests

---
+++ test/contacts.js
@@ -0,0 +1,30 @@
+describe('contacts', function () {
+
+  describe('POST /contacts', function () {
+    // creating a contacta
+    // relationship: from: ... to: ...
+    // don't create a duplicate contact (both directions)
+    // no contact to self can be created
+    it('todo');
+  });
+
+  describe('PATCH /contacts', function () {
+    // confirming the contact
+    it('todo');
+    // updating the reference and level of trust (0 - 4)
+    it('todo');
+  });
+
+  describe('GET', function () {
+    // read the contacts of a user
+    //
+    // give both directions of contact
+    // read info of one user
+    it('todo');
+  });
+
+  describe('DELETE', function () {
+    // delete a contact
+    it('todo');
+  });
+});
